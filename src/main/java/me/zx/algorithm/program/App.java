a0e332c49bac9ff291e1959c8f01ec61760f90db
zhangxin <zhangxin@192.168.0.103>
2017-07-24 23:11:13 +0800

    first commit

---
+++ src/main/java/me/zx/algorithm/program/App.java
@@ -0,0 +1,12 @@
+package me.zx.algorithm.program;
+
+/**
+ * Hello world.
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+	System.out.println("Hello World!");
+
+    }
+}
